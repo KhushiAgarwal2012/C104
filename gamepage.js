995bbcd3000ee8fbd8a14830afcc15e4e426f57a
KhushiAgarwal2012 <151986309+KhushiAgarwal2012@users.noreply.github.com>
2024-04-10 20:05:20 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,34 @@
+// generation of random value
+var y = Math.floor(Math.random() * 10 + 1);
+// count of attempts
+var guess = 1;
+// until hit
+var playername = document.getElementById("playername").innerHTML;
+// function for the number sent by the user
+function submit()
+{
+    var x = document.getElementById("guessField").value;
+
+    if (x == y)
+    {
+        alert("CONGRAGULATIONS!!!" + playername + "YOU GUESSED IT RIGHT IN" + guess + "GUESS");
+        guess = 1;
+    }
+
+    else if(x > y)
+    {
+        guess++;
+        alert("OOPS SORRY! TRY A SMALLER NUMBER");
+    }
+
+    else
+    {
+        guess++;
+        alert("OOPS SORRY! TRY A GREATER NUMBER"); 
+    }
+}
+
+function playAgain()
+{
+    var y = Math.floor(Math.random() * 10 + 1);
+}
